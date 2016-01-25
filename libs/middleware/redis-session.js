c4018d03e26ab91b98e177d537f515696c93b270
Nathanael Gethers <gethernm@gmail.com>
2016-01-24 23:03:10 -0600

    derp

--- libs/middleware/redis-session.js
+++ libs/middleware/redis-session.js
@@ -5,7 +5,9 @@ var RedisStore = require('connect-redis')(session);
 var redis = require('redis');
 
 module.exports = function (settings, redisConfig) {
+	console.log(settings.port, settings.host);
 	var client = redis.createClient(settings.port, settings.host);
+    // var client = redis.createClient(process.env.REDIS_URL);
 	redisConfig.client = client;
     settings.store = new RedisStore(redisConfig);
     return session(settings);

b854da8817c56d343c23aa3c12db2598df495a21
Nathanael Gethers <nathanael.gethers@premisehealth.com>
2016-02-02 10:57:11 -0600

    using redis url

--- libs/middleware/redis-session.js
+++ libs/middleware/redis-session.js
@@ -2,15 +2,23 @@
 
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
-var redis = require('redis');
+// var redis = require('redis');
+// Or you can specify a URL 
+var redis = require('redis-url');
 
 module.exports = function (settings, redisConfig) {
-	// var url = process.env.REDIS_URL || redisConfig.url;
-	var url = redisConfig.url;
-	console.log(url);
-	var client = redis.createClient(url, {port: redisConfig.port, host: redisConfig.host, password: redisConfig.password});
+	var url = process.env.REDIS_URL || redisConfig.url;
+	// var url = redisConfig.url;
+	var redisOptions = {
+		port: redisConfig.port, 
+		host: redisConfig.host, 
+		password: redisConfig.password
+	};
+	// Or you can specify a URL 
+    var client = redis.connect(url);
+	// var client = redis.createClient(url, redisOptions);
 	// var client = redis.createClient(url);
-	client.auth(redisConfig.password);
+	// client.auth(redisConfig.password);
 
 	redisConfig.client = client;
     settings.store = new RedisStore(redisConfig);
